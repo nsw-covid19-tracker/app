ea51ce20f300f89683484470fcb5e14520618cf1
Joshua <joshuaystang@gmail.com>
2020-09-25 11:38:58 +1000

    Tidy up dialog container

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -88,53 +88,69 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _showCaseInfo(Case myCase) {
-    final height = MediaQuery.of(context).size.height * 0.3;
-    final expiredText = myCase.isExpired ? ' (Expired)' : '';
     AwesomeDialog(
       context: context,
       animType: AnimType.SCALE,
       dialogType: myCase.isExpired ? DialogType.INFO : DialogType.WARNING,
-      body: Container(
-        height: height,
-        padding: kLayoutPadding,
-        child: FadingEdgeScrollView.fromScrollView(
-          child: ListView(
-            controller: _scrollController,
-            children: [
-              Text(
-                '${myCase.location}$expiredText',
-                style: Theme.of(context)
-                    .textTheme
-                    .subtitle1
-                    .apply(fontWeightDelta: 1),
-              ),
-              WidgetPaddingSm(),
-              Text('Dates', style: TextStyle(fontWeight: FontWeight.bold)),
-              Text(myCase.dates),
-              WidgetPaddingSm(),
-              RichText(
-                text: TextSpan(
-                  text: 'Suggested Action ',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '(If you visited this location at the times above)',
-                      style: Theme.of(context).textTheme.caption,
-                    ),
-                  ],
-                ),
-              ),
-              Text(myCase.action),
-              WidgetPaddingSm(),
-            ],
-          ),
-        ),
-      ),
+      body: _CaseInfo(myCase: myCase, controller: _scrollController),
       btnOkOnPress: () {},
       btnOkText: 'Close',
     )..show();
+  }
+}
+
+class _CaseInfo extends StatelessWidget {
+  final Case myCase;
+  final ScrollController controller;
+
+  const _CaseInfo({Key key, @required this.myCase, @required this.controller})
+      : assert(myCase != null),
+        assert(controller != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 0.3;
+    final expiredText = myCase.isExpired ? ' (Expired)' : '';
+
+    return Container(
+      height: height,
+      padding: kLayoutPadding,
+      child: FadingEdgeScrollView.fromScrollView(
+        child: ListView(
+          controller: controller,
+          children: [
+            Text(
+              '${myCase.location}$expiredText',
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1
+                  .apply(fontWeightDelta: 1),
+            ),
+            WidgetPaddingSm(),
+            Text('Dates', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(myCase.dates),
+            WidgetPaddingSm(),
+            RichText(
+              text: TextSpan(
+                text: 'Suggested Action ',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '(If you visited this location at the times above)',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ],
+              ),
+            ),
+            Text(myCase.action),
+            WidgetPaddingSm(),
+          ],
+        ),
+      ),
+    );
   }
 }
