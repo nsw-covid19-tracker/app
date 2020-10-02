dd4c9a8cdfcb71f93922bf3f551820b7e2223441
zeshuaro <joshuaystang@gmail.com>
2020-10-02 10:26:17 +1000

    Update/map widget (#22)
    * Change map to stateless widget
    
    * Tidy up map widget code

--- lib/home/widgets/map.dart
+++ lib/home/widgets/map.dart
@@ -1,22 +1,25 @@
 import 'dart:async';
 
+import 'package:covid_tracing/home/repo/repo.dart';
+import 'package:covid_tracing/home/widgets/case_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-class Map extends StatefulWidget {
+class Map extends StatelessWidget {
+  final ScrollController scrollController;
   final PanelController panelController;
-  final Set<Marker> markers;
+  final List<Case> cases;
 
-  const Map({Key key, this.markers, @required this.panelController})
-      : assert(panelController != null),
+  Map({
+    Key key,
+    this.cases,
+    @required this.scrollController,
+    @required this.panelController,
+  })  : assert(scrollController != null),
+        assert(panelController != null),
         super(key: key);
 
-  @override
-  _MapState createState() => _MapState();
-}
-
-class _MapState extends State<Map> {
   final _controller = Completer<GoogleMapController>();
   final _kGooglePlex = CameraPosition(
     target: LatLng(-33.868800, 151.209300),
@@ -31,8 +34,21 @@ class _MapState extends State<Map> {
       onMapCreated: (GoogleMapController controller) {
         _controller.complete(controller);
       },
-      markers: widget.markers,
-      onTap: (_) => widget.panelController.close(),
+      markers: cases != null ? _mapCasesToMarkers(context, cases) : null,
+      onTap: (_) => panelController.close(),
     );
+  }
+
+  Set<Marker> _mapCasesToMarkers(BuildContext context, List<Case> cases) {
+    final markers = <Marker>{};
+    for (var myCase in cases) {
+      markers.add(Marker(
+        markerId: MarkerId(myCase.location),
+        position: myCase.latLng,
+        onTap: () => CaseDialog.show(context, scrollController, myCase),
+      ));
+    }
+
+    return markers;
   }
 }
