7969e65fb691f2e53bcecdda183986d793670f0c
Joshua <joshuaystang@gmail.com>
2020-10-09 09:23:15 +1100

    Tidy up code

--- functions/fetch_data_old.py
+++ functions/fetch_data_old.py
@@ -28,11 +28,10 @@ def main():
         "https://data.nsw.gov.au/data/api/3/action/datastore_search?"
         "resource_id=5200e552-0afb-4bde-b20f-f8dd4feff3d7&limit=100"
     )
-    locations_ref = db.reference("locations")
-    cases_ref = db.reference("cases")
-    new_loc_count = new_case_count = updated_count = 0
+    page = 1
 
     while True:
+        logger.info(f"Fetching page {page} data")
         r = requests.get(url)
         result = r.json()["result"]
         records = result["records"]
@@ -52,45 +51,48 @@ def main():
                 continue
 
             postcode = postcode[0]
-            location_ref = locations_ref.child(postcode)
-
-            if location_ref.get() is None:
-                new_loc_count += 1
-                location_ref.set({"suburb": suburb})
-
-            case_dict = {
-                "postcode": postcode,
-                "suburb": suburb,
-                "location": case_loc,
-                "latitude": float(record["Latitude"]),
-                "longitude": float(record["Longitude"]),
-                "dates": record["Dates"],
-                "action": record["Action"],
-                "isExpired": record["Status"].lower() == "expired",
-            }
-
-            m = hashlib.sha384()
-            m.update(case_loc.encode("utf-8"))
-            key = m.hexdigest()
-            case_ref = cases_ref.child(key)
-            snapshot = case_ref.get()
-
-            if snapshot is None:
-                new_case_count += 1
-                case_ref.set(case_dict)
-            elif (
-                snapshot["dates"] != case_dict["dates"]
-                or snapshot["isExpired"] != case_dict["isExpired"]
-            ):
-                updated_count += 1
-                case_ref.update(case_dict)
-                logger.info(f"Updating {case_loc}")
+            set_location(postcode, suburb)
+            set_case(postcode, suburb, case_loc, record)
 
         url = base_url + result["_links"]["next"]
+        page += 1
 
-    logger.info(f"Added {new_loc_count} suburb(s)")
-    logger.info(f"Added {new_case_count} cases")
-    logger.info(f"Updated {updated_count} cases")
+
+def set_location(postcode, suburb):
+    locations_ref = db.reference("locations")
+    location_ref = locations_ref.child(postcode)
+
+    if location_ref.get() is None:
+        location_ref.set({"suburb": suburb})
+
+
+def set_case(postcode, suburb, case_loc, record):
+    case_dict = {
+        "postcode": postcode,
+        "suburb": suburb,
+        "location": case_loc,
+        "latitude": float(record["Latitude"]),
+        "longitude": float(record["Longitude"]),
+        "dates": record["Dates"],
+        "action": record["Action"],
+        "isExpired": record["Status"].lower() == "expired",
+    }
+
+    m = hashlib.sha384()
+    m.update(case_loc.encode("utf-8"))
+    key = m.hexdigest()
+
+    cases_ref = db.reference("cases")
+    case_ref = cases_ref.child(key)
+    snapshot = case_ref.get()
+
+    if snapshot is None:
+        case_ref.set(case_dict)
+    elif (
+        snapshot["dates"] != case_dict["dates"]
+        or snapshot["isExpired"] != case_dict["isExpired"]
+    ):
+        case_ref.update(case_dict)
 
 
 if __name__ == "__main__":
