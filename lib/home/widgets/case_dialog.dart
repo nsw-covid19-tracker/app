d2ac74c5fecf2a5f7b9515d639d160c42ce87f57
Joshua <joshuaystang@gmail.com>
2020-10-13 11:20:49 +1100

    Update dialog height

--- lib/home/widgets/case_dialog.dart
+++ lib/home/widgets/case_dialog.dart
@@ -29,7 +29,7 @@ class _CaseInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.4,
+      height: MediaQuery.of(context).size.height * 0.3,
       padding: kLayoutPadding,
       child: FadingEdgeScrollView.fromScrollView(
         child: ListView(
