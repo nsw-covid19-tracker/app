97ee21d0a0f95e509ce275ff27f7b0dbf61b30eb
Joshua <joshuaystang@gmail.com>
2020-10-15 14:18:53 +1100

    Update deploy commands

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -4,11 +4,13 @@ gcloud functions deploy fetch-data \
     --runtime python37 \
     --trigger-resource fetch-data \
     --trigger-event google.pubsub.topic.publish \
+    --ingress-settings internal-only \
+    --memory 128MB \
     --timeout 180s
 
 # Schedule data fetch
 gcloud scheduler jobs create pubsub daily_scrape \
-    --schedule "0 6 * * *" \
+    --schedule "30 10 * * *" \
     --time-zone "Australia/Sydney" \
     --topic fetch-data \
     --message-body "Fetch NSW COVID-19 Case Locations"
