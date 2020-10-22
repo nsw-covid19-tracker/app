9cb99d2fbb6c2b131f274404de0a88ee6f57a265
Joshua <joshuaystang@gmail.com>
2020-10-22 13:01:32 +1100

    Fix map dragging on web

--- lib/home/widgets/map.dart
+++ lib/home/widgets/map.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nsw_covid_tracker/home/bloc/home_bloc.dart';
 import 'package:nsw_covid_tracker/home/repo/repo.dart';
@@ -10,9 +11,15 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapWidget extends StatefulWidget {
   final ScrollController scrollController;
   final Function onMapTap;
+  final bool isShowMap;
 
-  MapWidget({Key key, @required this.scrollController, this.onMapTap})
-      : assert(scrollController != null),
+  MapWidget({
+    Key key,
+    @required this.scrollController,
+    this.onMapTap,
+    isShowMap = true,
+  })  : assert(scrollController != null),
+        isShowMap = isShowMap || !kIsWeb,
         super(key: key);
 
   @override
@@ -52,18 +59,22 @@ class _MapWidgetState extends State<MapWidget> {
         var cases = <Case>[];
         if (state is HomeSuccess) cases = state.casesResult;
 
-        return GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: _kGooglePlex,
-          myLocationEnabled: false,
-          myLocationButtonEnabled: false,
-          zoomControlsEnabled: false,
-          onMapCreated: (GoogleMapController controller) {
-            _completer.complete(controller);
-          },
-          markers: cases.isNotEmpty ? _mapCasesToMarkers(context, cases) : null,
-          onTap: (_) => widget.onMapTap?.call(),
-        );
+        return widget.isShowMap
+            ? GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: _kGooglePlex,
+                myLocationEnabled: false,
+                myLocationButtonEnabled: false,
+                zoomControlsEnabled: false,
+                onMapCreated: (GoogleMapController controller) {
+                  if (!_completer.isCompleted) _completer.complete(controller);
+                },
+                markers: cases.isNotEmpty
+                    ? _mapCasesToMarkers(context, cases)
+                    : null,
+                onTap: (_) => widget.onMapTap?.call(),
+              )
+            : SizedBox.shrink();
       },
     );
   }
