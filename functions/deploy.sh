ca22098490d50077c5d663b009a3c678b8246c23
zeshuaro <joshuaystang@gmail.com>
2020-10-24 12:39:07 +1100

    Update deploy script

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -10,7 +10,7 @@ gcloud functions deploy fetch-data \
 
 # Schedule data fetch
 gcloud scheduler jobs create pubsub daily_scrape \
-    --schedule "* 6,18 * * *" \
+    --schedule "0 6,12,18 * * *" \
     --time-zone "Australia/Sydney" \
     --topic fetch-data \
     --message-body "Fetch NSW COVID-19 Case Locations"
