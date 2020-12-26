78b23bc39fd9f20e679dc1ab3adcf497edc0be7c
zeshuaro <joshuaystang@gmail.com>
2020-12-26 12:05:18 +0000

    Deploying to gh-pages from  @ 9da4a94766983f628436c7eab404579c364eb9f9 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,8 +22,8 @@ const RESOURCES = {
 "assets/AssetManifest.json": "eaec39849164cbd10f19b91ee359c75e",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"index.html": "b11b045b7d75f3f0928fa0c74aa21121",
-"/": "b11b045b7d75f3f0928fa0c74aa21121",
+"index.html": "c63f10eb06a8d42925df490252d5feb3",
+"/": "c63f10eb06a8d42925df490252d5feb3",
 "version.json": "027b355b0e5da5fa4674c9f43f08af3a",
 "main.dart.js": "58094fa3f89f2e8d9cefdfe12cc6a314",
 "favicon.png": "b1d678a6c81adb848e794bd202595958"
